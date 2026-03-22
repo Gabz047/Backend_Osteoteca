b6410c2c684720a13826e27f7ba1490f6885c2cb
Gabz047 <gabe1612mail@gmail.com>
2026-03-22 17:01:43 -0300

    FIX: Repetead configuration

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,16 +1,6 @@
 import { Sequelize } from 'sequelize'
-import dotenv from 'dotenv'
+import config from '../config/database'
 
-dotenv.config()
-
-const sequelize = new Sequelize({
-  dialect: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  logging: process.env.NODE_ENV === 'development' ? console.log : false,
-})
+const sequelize = new Sequelize(config)
 
 export default sequelize
